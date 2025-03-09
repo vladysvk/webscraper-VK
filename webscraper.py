1fbca47e185aec00acbb352704334259f1ba8cdd
Vladyslav <vladkuzil07@gmail.com>
2025-03-09 16:46:50 +0100

    User input added

--- webscraper.py
+++ webscraper.py
@@ -1,5 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get("https://www.ceneo.pl/")
-print(html_text)
+initial_link = "https://www.ceneo.pl/"
+modified_link = ""
+
+number = input("Enter product number: ")
+
+modified_link = initial_link + number
+html_text = requests.get(modified_link).text
+soup = BeautifulSoup(html_text, "lxml")
+print(soup.prettify())
